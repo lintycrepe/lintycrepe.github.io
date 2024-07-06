26b3e615ae667a3606505b5116043512b168a973
Casey Robinson <casey@lintycrepe.com>
2024-07-06 18:51:27 -0400

    link to github repo in footer

--- Sources/LintyCrepeCom/Theme+LintyCrepe.swift
+++ Sources/LintyCrepeCom/Theme+LintyCrepe.swift
@@ -217,11 +217,8 @@ private struct SiteFooter: Component {
     var body: Component {
         Footer {
             Paragraph {
-                Text("Generated using ")
-                Link("Publish", url: "https://github.com/johnsundell/publish")
-            }
-            Paragraph {
                 Link("RSS feed", url: "/feed.rss")
+                Link("Git Repository", url: "https://github.com/lintycrepe/lintycrepe.github.io")
             }
         }
     }
